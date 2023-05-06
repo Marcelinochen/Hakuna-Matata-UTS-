efbb4f5ec4a31bdea82762cf84a2f7c7cea45e3d
Marcelino <marcelinochen92@gmail.com>
2023-05-06 08:53:22 -0700

    Gabung Halaman Daftar Menu & Daftar Pelanggan

--- scpt.js
+++ scpt.js
@@ -4,67 +4,67 @@ let food = [
         name:`Americano`,
         stok: 10,
         harga: 15000,
-        image: 'americano.jpeg'
+        image: 'https://foto.kontan.co.id/Ohrz3SH23BdMi7J7XLG6JgDgO6w=/smart/filters:format(webp)/2023/01/26/1632610923p.jpg'
     },
     {
         name:`Expresso`,
         stok: 10,
         harga: 60000,
-        image: 'expresso.jpeg'
+        image:  'https://kopikita.id/wp-content/uploads/2022/12/espresso-vs-expresso.jpg'
     },
     {
         name:`Nasi Goreng`,
         stok: 10,
         harga: 2500,
-        image: 'nasgor.jpeg'
+        image:  'https://awsimages.detik.net.id/community/media/visual/2022/08/07/resep-nasi-goreng-ayam-dan-ebi_43.jpeg?w=1200'
     },
     {
         name:`Kentang Goreng`,
         stok: 10,
         harga: 70000,
-        image: 'kengo.jpeg'
+        image:  'https://aromarasa.com/wp-content/uploads/2021/07/resep-kentang-goreng-crispy-ala-mcd-sederhana.jpg'
 
     },
     {
         name:`Es Teh Manis`,
         stok: 10,
         harga: 20000,
-        image: 'mandi.jpeg'
+        image:  'https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2021/06/15093247/Ketahui-Fakta-Es-Teh-Manis.jpg'
 
     },
     {
         name:`Tiramisu`,
         stok: 10,
         harga: 20000,
-        image: 'tira.jpeg'
+        image:  'https://cdn0-production-images-kly.akamaized.net/aj6sjLfKGfQah9ibj8aJCs6m7d8=/1x54:1000x617/1200x675/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/4078274/original/018227100_1656925103-shutterstock_1141971746.jpg'
 
     },
     {
         name:`Mie`,
         stok: 10,
         harga: 20000,
-        image: 'mie.jpeg'
+        image: 'https://kbu-cdn.com/dk/wp-content/uploads/mie-goreng-korea.jpg'
 
     },
     {
         name:`Croffle`,
         stok: 10,
         harga: 20000,
-        image: 'crof.jpeg'
+        image:  'https://img.okezone.com/content/2021/06/04/298/2420446/resep-akhir-pekan-croffle-camilan-yang-lagi-ngetren-di-korea-selatan-94RkEgQ5nN.jpg'
 
     },
     {
         name:`Roti Bakar`,
         stok: 10,
         harga: 20000,
-        image: 'robar.jpeg'
+        image: 'https://img-global.cpcdn.com/recipes/d463a2856962f367/1200x630cq70/photo.jpg'
 
     },
     {
         name:`Pasta`,
         stok: 10,
         harga: 20000,
-        image: 'pasta.jpeg'
+        image: 'https://www.foodandwine.com/thmb/97PY4E6Wk95IYv1_8pDZvBEi0Uw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/cream-tomato-rigatoni-FT-RECIPE1020-139fb3fa52574e8bb06f98e7fa3e4f1e.jpg'
 
     },
 ]
